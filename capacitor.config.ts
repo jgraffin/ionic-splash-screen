aee4315e5174cd78286d5a86fb8700c3451eb981
Julio Ferreira <julio.ferreira@gs.liferay.com>
2022-03-10 00:10:16 -0300

    Add Capacitor configurations

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,27 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic splash screen',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "io.ionic.starter",
+  appName: "ionic splash screen",
+  webDir: "build",
+  bundledWebRuntime: true,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 100000,
+      launchAutoHide: true,
+      backgroundColor: "#290707",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "inverse",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#781DB9",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
